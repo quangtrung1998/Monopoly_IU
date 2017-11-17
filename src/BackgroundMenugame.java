eb14a0b0202c96a99690129f9200307d498652b4
quangtrung1998 <33691470+quangtrung1998@users.noreply.github.com>
2017-11-17 23:41:35 +0700

    thêm ghi chú
    thêm ghi chú

--- src/BackgroundMenugame.java
+++ src/BackgroundMenugame.java
@@ -4,5 +4,30 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class BackgroundMenugame extends JPanel{
-	
+	private static BuyHouse buyhouse;
+	/**
+	 * 	Paint again
+	 * @param 	
+	 * @return 	clear table and paint again
+	 */
+	public BackgroundMenugame() {
+		repaint();
+	}
+	/**
+	 * 	Draw Image
+	 * @param 	Graphics g , address image, location horizontal , location vertical , width , height 
+	 * @return 	show the image in the frame
+	 */
+	private void paint1(Graphics g,String path,int x,int y,int width,int height) { // create function to save time 
+		g.drawImage(new ImageIcon(path).getImage(),x, y,width,height,null);
+	}
+	/**
+	 * 	Draw Image
+	 * @param 	Graphics g , address image, location horizontal , location vertical , width , height 
+	 * @return 	show the image in the frame
+	 */
+	public void paint(Graphics g)
+    {
+		paint1(g,"menugame.jpg",0,0,800,800);
+    }
 }
