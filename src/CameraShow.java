21e4b269e1995827e24a2786a48b7ff9e4dd97bb
quangtrung1998 <33691470+quangtrung1998@users.noreply.github.com>
2017-11-17 19:31:17 +0700

    CreateCameraShow
    mục đích để quan sát vị trí người chơi

--- src/CameraShow.java
+++ src/CameraShow.java
@@ -4,4 +4,18 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class CameraShow extends JPanel{
+	private static int changeimage;
+	public CameraShow() {
+		repaint();
+	}
+	public void change(int changeimage) {	
+		this.changeimage=changeimage;
+	}
+	private void paint1(Graphics g,String path,int x,int y,int width,int height) { // create function to save time 
+		g.drawImage(new ImageIcon(path).getImage(),x, y,width,height,null);
+	}
+	public void paint(Graphics g)
+    {
+		paint1(g,changeimage+".jpg",0,0,340,310);
+    }
 }
