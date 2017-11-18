f933bbcb99dcf612a4cdeb097aa7392aa16d80b8
Khương Minh Ngọc <30364166+shithead1604@users.noreply.github.com>
2017-11-18 17:53:52 +0700

    update ChangeDice

--- src/ChangeDice.java
+++ src/ChangeDice.java
@@ -3,7 +3,22 @@ import java.util.Random;
 
 
 public class ChangeDice {
+	private static int diceleft,diceright;
 	
+	private static int rand(int min, int max) {
+	        try {
+	            Random rn = new Random();
+	            int range = max - min + 1;
+	            int randomNum = min + rn.nextInt(range);
+	            return randomNum;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            return -1;
+	        }
+	    }
+	public static int getDiceleft() {
+		return diceleft;
+	}
 }
 
 
