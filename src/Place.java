18caad62d2fcab6bbf378ec93d19ad5abf1a474d
tuanngociuit98 <33689489+tuanngociuit98@users.noreply.github.com>
2017-11-18 21:11:56 +0700

    update Place

--- src/Place.java
+++ src/Place.java
@@ -3,7 +3,44 @@
 import java.awt.Color;
 
 public class Place {
-	
+	public int locate,priceini;
+	public static int price1;
+	public static int price2;
+	public static int price3;
+	public static int villa;
+	public int nameplace;
+	private Color color;
+	public void setLocate(int locate) {
+		this.locate = locate;
+	}
+	public int getNameplace() {
+		return nameplace;
+	}
+	public int getPriceini() {
+		return priceini;
+	}
+	public int getPrice1() {
+		return price1;
+	}
+	public int getPrice2() {
+		return price2;
+	}
+	public int getPrice3() {
+		return price3;
+	}
+	public int getVilla() {
+		return villa;
+	}
+
+	public int getLocate() {
+		return locate;
+	}	
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
 	
 
 }
