6b9263b8dbb2653fd606232f363280e3bdb812a7
quangtrung1998 <33691470+quangtrung1998@users.noreply.github.com>
2017-11-24 22:21:16 +0700

    create interface Image and ShowCamera

---
+++ src/ShowCamera.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public interface ShowCamera {
+	/**
+	 * Change
+	 * @param   data was put 
+	 * @return 	the change of camera show and paint again
+	 */
+	public void change(int data);
+	/**
+	 * Show the frame and image in the frame
+	 * @return 	the frame and image in the frame
+	 */
+	public void Show();
+}
