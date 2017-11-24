867e42a2c719486467d40c5f411e7b2eb15854f3
ducdinhtri0203 <33689925+ducdinhtri0203@users.noreply.github.com>
2017-11-24 22:24:29 +0700

    Implements Image

--- src/BackgroundMenugame.java
+++ src/BackgroundMenugame.java
@@ -3,9 +3,7 @@ import java.awt.Graphics;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class BackgroundMenugame extends JPanel{
-	private static BuyHouse buyhouse;
-
+public class BackgroundMenugame extends JPanel implements Image{
 	/**
 	 * 	Paint again
 	 * @param 	
@@ -19,23 +17,23 @@ public class BackgroundMenugame extends JPanel{
 	 * @param 	Graphics g , address image, location horizontal , location vertical , width , height 
 	 * @return 	show the image in the frame
 	 */
-	private void paint1(Graphics g,String path,int x,int y,int width,int height) { // create function to save time 
+	@Override
+	public void paint1(Graphics g,String path,int x,int y,int width,int height) { // create function to save time 
 		g.drawImage(new ImageIcon(path).getImage(),x, y,width,height,null);
 	}
 	/**
 	 * 	Draw Image
-	 * @param 	Graphics g , address image, location horizontal , location vertical , width , height 
+	 * @param 	Graphics g 
 	 * @return 	show the image in the frame
 	 */
-
-	public BackgroundMenugame() {
-		repaint();
-	}
-	private void paint1(Graphics g,String path,int x,int y,int width,int height) { // create function to save time 
-		g.drawImage(new ImageIcon(path).getImage(),x, y,width,height,null);
-	}
+	@Override
 	public void paint(Graphics g)
     {
 		paint1(g,"menugame.jpg",0,0,800,800);
     }
+	@Override
+	public void change(int changeimage) {
+		// TODO Auto-generated method stub
+		
+	}
 }
