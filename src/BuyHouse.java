f6284a9fca20986210fe3079a26f395dd90ea641
quangtrung1998 <33691470+quangtrung1998@users.noreply.github.com>
2017-12-01 00:03:54 +0700

    tạo cho máy tự động mua và nâng cấp nhà

--- src/BuyHouse.java
+++ src/BuyHouse.java
@@ -18,80 +18,144 @@ public class BuyHouse extends Place{ //HouseOperation
 		 {
 				
 		 }
+		 else if(a==5||a==15||a==25||a==35) {
+			 if(player==1) {
+				if(buy[1][a]==0) {
+					 Object[] options = {list[5],list[6]};
+					 int n = JOptionPane.showOptionDialog(game,list[7],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+					 if(n==JOptionPane.YES_OPTION)
+						 {
+						 	MainGame.inialmoney1-=Place.getPriceini();
+						 	buy[1][a]=1;
+						 }
+				}
+				else {
+					int totalprice=0;
+					for(int i=5;i<=35;i+=10) {
+						if(buy[2][a]==2)
+							totalprice++;
+					}
+					if(totalprice==1)
+						MainGame.inialmoney1-=Place.getPrice1();
+					else if(totalprice==2)
+						MainGame.inialmoney1-=Place.getPrice2();
+					else if(totalprice==3)
+						MainGame.inialmoney1-=Place.getPrice3();
+					else if(totalprice==4)
+						MainGame.inialmoney1-=Place.getVilla();
+				}
+			 }
+			 else if(player==2) {
+				 if(buy[2][a]==0) {
+					 if(MainGame.inialmoney2>Place.getPriceini()+200)
+						 {
+						 	MainGame.inialmoney2-=Place.getPriceini();
+						 	buy[2][a]=1;
+						 }
+				}
+				else {
+					int totalprice=0;
+					for(int i=5;i<=35;i+=10) {
+						if(buy[1][a]==2)
+							totalprice++;
+					}
+					if(totalprice==1)
+						MainGame.inialmoney2-=Place.getPrice1();
+					else if(totalprice==2)
+						MainGame.inialmoney2-=Place.getPrice2();
+					else if(totalprice==3)
+						MainGame.inialmoney2-=Place.getPrice3();
+					else if(totalprice==4)
+						MainGame.inialmoney2-=Place.getVilla();
+				}
+			 }
+		 }
 		 else if(buy[player][a]==0&&buy[check][a]!=0)
 		 {
 			 Object[] options = {list[3],list[4]};
 			 if(buy[check][a]==1)
 			 {	
 				 
-				 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice1()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
-				 if(player==2&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice1();
-					 MainGame.inialmoney2-=Place.getPrice1();
+				 if(player==1) {
+					 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice1()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+					 if(n==JOptionPane.YES_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice1();
+						 MainGame.inialmoney1-=Place.getPrice1();
+					 }
+					 else if(n==JOptionPane.NO_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice1();
+						 MainGame.inialmoney1 -=Place.getPrice1();
+					 }
 				 }
-				 else if(player==2&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice1();
-					 MainGame.inialmoney2 =0;
-				 }
-				 else if(player==1&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice1();
-					 MainGame.inialmoney1-=Place.getPrice1();
-				 }
-				 else if(player==1&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice1();
-					 MainGame.inialmoney1 =0;
+				 if(player==2) {
+					 if(MainGame.inialmoney2>=Place.getPrice1())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice1();
+						 MainGame.inialmoney2-=Place.getPrice1();
+					 }
+					 else if(MainGame.inialmoney2<Place.getPrice1())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice1();
+						 MainGame.inialmoney2 -=Place.getPrice1();
+					 }
 				 }
 			 }
 			 else if(buy[check][a]==2)
 			 {
-				 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice2()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
-				 if(player==2&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice2();
-					 MainGame.inialmoney2-=Place.getPrice2();
+				 if(player==1) {
+					 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice2()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+					 if(n==JOptionPane.YES_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice2();
+						 MainGame.inialmoney1-=Place.getPrice2();
+					 }
+					 else if(n==JOptionPane.NO_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice2();
+						 MainGame.inialmoney1-=Place.getPrice2();
+					 }
 				 }
-				 else if(player==2&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice2();
-					 MainGame.inialmoney2 =0;
-				 }
-				 else if(player==1&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice2();
-					 MainGame.inialmoney1-=Place.getPrice2();
-				 }
-				 else if(player==1&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice2();
-					 MainGame.inialmoney1 =0;
+				 if(player==2) {
+					 if(MainGame.inialmoney2>=Place.getPrice2())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice2();
+						 MainGame.inialmoney2-=Place.getPrice2();
+					 }
+					 else if(MainGame.inialmoney2<Place.getPrice2())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice2();
+						 MainGame.inialmoney2 -=Place.getPrice2();
+					 }
 				 }
 			 }
 			 else if(buy[check][a]==3)
 			 {
-				 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice3()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
-				 if(player==2&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice3();
-					 MainGame.inialmoney2-=Place.getPrice3();
+				 if(player==1) {
+					 int n = JOptionPane.showOptionDialog(game,list[0]+Place.getPrice3()+list[1],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+					 if(n==JOptionPane.YES_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice3();
+						 MainGame.inialmoney1-=Place.getPrice3();
+					 }
+					 else if(n==JOptionPane.NO_OPTION)
+					 {
+						 MainGame.inialmoney2+=Place.getPrice3();
+						 MainGame.inialmoney1 -=Place.getPrice3();
+					 }
 				 }
-				 else if(player==2&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney1+=Place.getPrice3();
-					 MainGame.inialmoney2 =0;
-				 }
-				 else if(player==1&&n==JOptionPane.YES_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice3();
-					 MainGame.inialmoney1-=Place.getPrice3();
-				 }
-				 else if(player==1&&n==JOptionPane.NO_OPTION)
-				 {
-					 MainGame.inialmoney2+=Place.getPrice3();
-					 MainGame.inialmoney1 =0;
+				 if(player==2) {
+					 if(MainGame.inialmoney2>=Place.getPrice3())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice3();
+						 MainGame.inialmoney2-=Place.getPrice3();
+					 }
+					 else if(MainGame.inialmoney2<Place.getPrice3())
+					 {
+						 MainGame.inialmoney1+=Place.getPrice3();
+						 MainGame.inialmoney2-=Place.getPrice3();
+					 }
 				 }
 			 }
 			 else
@@ -102,51 +166,22 @@ public class BuyHouse extends Place{ //HouseOperation
 		 }
 		 else if(buy[player][a]==0)
 		 {
-			 Object[] options = {list[5],list[6]};
-			 int n = JOptionPane.showOptionDialog(game,list[7],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
-			 if(n==JOptionPane.YES_OPTION)
-			 {
-				
-				 	Object[] options1 = {list[8],list[9],list[10]};
-				 	int checkcombo =0;
-				 	if(buy[player][a]!=3)
-				 	{
-				 	int n1 = JOptionPane.showOptionDialog(game,list[14],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[2]);
-				 	if(n1==JOptionPane.YES_OPTION)
-				 		{
-				 			buy[player][a]=1;
-				 			
-				 			if(player==1)
-				 			{
-				 				MainGame.inialmoney1-=Place.getPrice1();
-				 				if(MainGame.mode.getSize()==1)
-				 					MainGame.mode.addElement(a);
-				 				else
-				 				{
-				 					for(int i=1;i<MainGame.mode.getSize();i++) {
-				 						checkcombo = (int) MainGame.mode.getElementAt(i);
-				 						if(a==checkcombo)
-				 							break;
-				 						if(a>checkcombo)
-				 							{
-				 								MainGame.mode.insertElementAt(a, i);
-				 								break;
-				 							}
-				 					}
-				 				}
-				 			}
-				 			else if(player==2)
-				 			MainGame.inialmoney2-=Place.getPrice1();
-					 		
-				 		}
-				 	else if(n1==JOptionPane.NO_OPTION)
-				 		{
-				 			buy[player][a]=2;
-				 	
-				 			if(player==1)
-					 			{
-				 					MainGame.inialmoney1-=Place.getPrice2();
-				 					if(MainGame.mode.getSize()==1)
+			 if(player==1) {
+				 Object[] options = {list[5],list[6]};
+				 int n = JOptionPane.showOptionDialog(game,list[7],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+				 if(n==JOptionPane.YES_OPTION)
+				 {
+					
+					 	Object[] options1 = {list[8],list[9],list[10]};
+					 	int checkcombo =0;
+					 	if(buy[player][a]!=3)
+					 	{
+					 	int n1 = JOptionPane.showOptionDialog(game,list[14],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[2]);
+					 	if(n1==JOptionPane.YES_OPTION)
+					 		{
+					 			buy[player][a]=1;
+					 				MainGame.inialmoney1-=Place.getPrice1();
+					 				if(MainGame.mode.getSize()==1)
 					 					MainGame.mode.addElement(a);
 					 				else
 					 				{
@@ -161,106 +196,140 @@ public class BuyHouse extends Place{ //HouseOperation
 					 							}
 					 					}
 					 				}
-					 			}
-					 			else if(player==2)
-					 			MainGame.inialmoney2-=Place.getPrice2();
-				 		}
-				 	else if(n1==JOptionPane.CANCEL_OPTION)
-				 		{
-				 			buy[player][a]=3;
-				 		
-				 			if(player==1)
-					 			{
-				 					MainGame.inialmoney1-=Place.getPrice3();
-				 					if(MainGame.mode.getSize()==1)
-					 					MainGame.mode.addElement(a);
-					 				else
-					 				{
-					 					for(int i=1;i<MainGame.mode.getSize();i++) {
-					 						checkcombo = (int) MainGame.mode.getElementAt(i);
-					 						if(a==checkcombo)
-					 							break;
-					 						if(a>checkcombo)
-					 							{
-					 								MainGame.mode.insertElementAt(a, i);
-					 								break;
-					 							}
-					 					}
-					 				}
-					 			}
-					 			else if(player==2)
-					 			MainGame.inialmoney2-=Place.getPrice3();
-				 		}
-				 	}	 				 	
+					 			
+					 		}
+					 	else if(n1==JOptionPane.NO_OPTION)
+					 		{
+					 			buy[player][a]=2;
+					 					MainGame.inialmoney1-=Place.getPrice2();
+					 					if(MainGame.mode.getSize()==1)
+						 					MainGame.mode.addElement(a);
+						 				else
+						 				{
+						 					for(int i=1;i<MainGame.mode.getSize();i++) {
+						 						checkcombo = (int) MainGame.mode.getElementAt(i);
+						 						if(a==checkcombo)
+						 							break;
+						 						if(a>checkcombo)
+						 							{
+						 								MainGame.mode.insertElementAt(a, i);
+						 								break;
+						 							}
+						 					}
+						 				}
+						 			
+					 		}
+					 	else if(n1==JOptionPane.CANCEL_OPTION)
+					 		{
+					 			buy[player][a]=3;
+					 					MainGame.inialmoney1-=Place.getPrice3();
+					 					if(MainGame.mode.getSize()==1)
+						 					MainGame.mode.addElement(a);
+						 				else
+						 				{
+						 					for(int i=1;i<MainGame.mode.getSize();i++) {
+						 						checkcombo = (int) MainGame.mode.getElementAt(i);
+						 						if(a==checkcombo)
+						 							break;
+						 						if(a>checkcombo)
+						 							{
+						 								MainGame.mode.insertElementAt(a, i);
+						 								break;
+						 							}
+						 					}
+						 				}
+						 			
+					 		}
+					 	}	 				 	
+				 }
 			 }
+			 else if(player==2) {
+		 			if(MainGame.inialmoney2>Place.getPrice3()+500) {
+		 				MainGame.inialmoney2-=Place.getPrice3();
+		 				buy[player][a]=3;
+		 			}
+		 			else if(MainGame.inialmoney2>Place.getPrice2()+200) {
+		 				MainGame.inialmoney2-=Place.getPrice2();
+		 				buy[player][a]=2;
+		 			}
+		 			else if(MainGame.inialmoney2>Place.getPrice1()+100) {
+		 				MainGame.inialmoney2-=Place.getPrice1();
+		 				buy[player][a]=1;
+		 			}
+				}
 		 }
 		 else if(buy[player][a]!=0&&buy[player][a]<=4) {
 			 if(buy[player][a]==1) {
-				 Object[] options1 = {list[9],list[10],list[11]};
-				 	int n1 = JOptionPane.showOptionDialog(game,list[13],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[2]);
-				 	if(n1==JOptionPane.YES_OPTION)
-			 		{
-			 			buy[player][a]=2;
-			 			if(player==1)
-			 			{
-			 				MainGame.inialmoney1-=Place.getPrice1();
-			 			}
-			 			else if(player==2)
-			 			MainGame.inialmoney2-=Place.getPrice1();
-				 		
-			 		}
-				 	else if(n1==JOptionPane.NO_OPTION)
-			 		{
-			 			buy[player][a]=2;
-			 			if(player==1)
-				 			{
-			 					MainGame.inialmoney1-=Place.getPrice2();
-				 			}
-				 			else if(player==2)
-				 			MainGame.inialmoney2-=Place.getPrice2();
-			 		}
-				 	else if(n1==JOptionPane.CANCEL_OPTION)
-			 		{
-			 			
-			 		}
+				if(player==1) {
+					 Object[] options1 = {list[9],list[10],list[11]};
+					 	int n1 = JOptionPane.showOptionDialog(game,list[13],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[2]);
+					 	if(n1==JOptionPane.YES_OPTION)
+				 		{
+				 			buy[player][a]=2;
+				 			MainGame.inialmoney1-=Place.getPrice1();
+				 		}
+					 	else if(n1==JOptionPane.NO_OPTION)
+				 		{
+				 			buy[player][a]=2;
+				 			MainGame.inialmoney1-=Place.getPrice1()*2;		
+				 		}
+					 	else if(n1==JOptionPane.CANCEL_OPTION)
+				 		{
+				 			
+				 		}
+				}
+				else if(player==2) {
+					if(MainGame.inialmoney2>Place.getPrice1()*2+500) {
+		 				MainGame.inialmoney2-=Place.getPrice1()*2;
+		 				buy[player][a]=3;
+		 			}
+		 			else if(MainGame.inialmoney2>Place.getPrice1()+200) {
+		 				MainGame.inialmoney2-=Place.getPrice1();
+		 				buy[player][a]=2;
+		 			}
+				}
 			 }
 			 else if(buy[player][a]==2) {
+			if(player==1) {
 				 Object[] options1 = {list[10],list[11]};
 				 	int n1 = JOptionPane.showOptionDialog(game,list[13],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[1]);
 				 	if(n1==JOptionPane.YES_OPTION)
 			 		{
 			 			buy[player][a]=3;
-			 			if(player==1)
-			 			{
-			 				MainGame.inialmoney1-=Place.getPrice1();
-			 			}
-			 			else if(player==2)
-			 			MainGame.inialmoney2-=Place.getPrice1();
-				 		
+			 			MainGame.inialmoney1-=Place.getPrice1();
 			 		}
 				 	else if(n1==JOptionPane.CANCEL_OPTION)
 			 		{
 			 			
 			 		}
-			 }
+			}
+			else if(player==2) {
+				if(MainGame.inialmoney2>Place.getPrice1()+200) {
+	 				MainGame.inialmoney2-=Place.getPrice1();
+	 				buy[player][a]=3;
+	 			}
+			}
+		}
 			 else {
-				 Object[] options1 = {list[12],list[11]};
-				 	int n1 = JOptionPane.showOptionDialog(game,list[13],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[1]);
-				 	if(n1==JOptionPane.YES_OPTION)
-			 		{
-			 			buy[player][a]=4;
-			 			if(player==1)
-			 			{
-			 				MainGame.inialmoney1-=Place.getPrice1();
-			 			}
-			 			else if(player==2)
-			 			MainGame.inialmoney2-=Place.getPrice1();
-				 		
-			 		}
-				 	else if(n1==JOptionPane.CANCEL_OPTION)
-			 		{
-			 			
-			 		}
+				if(player==1) {
+					 Object[] options1 = {list[12],list[11]};
+					 	int n1 = JOptionPane.showOptionDialog(game,list[13],list[2],JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options1,options1[1]);
+					 	if(n1==JOptionPane.YES_OPTION)
+				 		{
+				 			buy[player][a]=4;
+				 			MainGame.inialmoney1-=Place.getPrice1();
+				 		}
+					 	else if(n1==JOptionPane.CANCEL_OPTION)
+				 		{
+				 			
+				 		}
+				}
+				else if(player==2) {
+					if(MainGame.inialmoney2>Place.getPrice1()+200) {
+		 				MainGame.inialmoney2-=Place.getPrice1();
+		 				buy[player][a]=4;
+		 			}
+				}
 			 }
 		 }
 		
